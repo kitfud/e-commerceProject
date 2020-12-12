337a78ce0ad57134422572147da9e75bb7a1f9ad
Kit Fuderich <kitfuderich@gmail.com>
2020-12-11 16:58:51 -0800

    commerce.js api backend connected

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,14 +4,11 @@ import {Grid} from '@material-ui/core';
 import Product from './Product/Product'
 import useStyles from './styles'
 
-const products = [
-    { id:1, name:'Colonized Agar Plate [Shitake Mushroom]', description:'fully colonized agar plate', price: '$8',image:'https://lh3.googleusercontent.com/pw/ACtC-3eLa5kp3B5__Hq7tK8ZLbo_QU7zfqFe6BvIjZXZEvHIMfg0qssKITjVZYNzcktfaJNsaqKHA9eqwBd_GSpycM6WMy4tJZa-bMaLtpv9m6v0AIRTeofB3fkUdOfV1cHyO-RC_D5hoQ-dhOCKu8tbMYsS=w508-h640-no?authuser=0'},
-    { id:1, name:'Colonized Agar Plate [Golden Oyster]', description:'fully colonized agar plate', price:'$8',image:'https://lh3.googleusercontent.com/pw/ACtC-3eMMKfremA4u4PHAbrFBJ1JyXj8ZfRErXderzsTeTo5FoRzk-Mjwv_dEqu-XpYG9GVsvy_trmfgtz0ZASO2o6J6LJJMWKgRz1kMp0sEF6KdlPtZNdQfLBwZ0gkxYNgnTlu17OGVpndqYIwh_ZbZ9aIl=w480-h640-no?authuser=0'},
-];
 
 
-const Products = () => {
+const Products = ({products}) => {
     const classes = useStyles();
+
 
     return (
 <main className = {classes.content}>
